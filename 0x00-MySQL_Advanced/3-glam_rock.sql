61b8594c0667f1236b6da746a264ba68c96aac48
brandonladen <brandonladen486@gmail.com>
2024-07-11 10:45:53 +0000

    creation of triggers

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,15 @@
+-- A script that lists all bands
+-- with Glam rock
+
+SELECT 
+	band_name,
+	CASE
+		WHEN split IS NULL THEN 2022 - formed
+		ELSE split - formed
+	END AS lifespan
+FROM 
+	metal_bands
+WHERE 
+	style LIKE '%Glam rock%'
+ORDER BY
+	lifespan DESC;
